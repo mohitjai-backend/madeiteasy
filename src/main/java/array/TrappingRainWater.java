4ac6f28e83b2840a3d42b4d661fe8454000da2d1
mohitjain2894 <mohit.jain@unicommerce.com>
2021-06-14 02:15:47 +0530

    Trapping rain water

--- src/main/java/array/TrappingRainWater.java
+++ src/main/java/array/TrappingRainWater.java
@@ -18,17 +18,13 @@ public class TrappingRainWater {
         for (int i = 1; i < n; i++) {
             left[i] = Math.max(left[i - 1], arr[i]);
         }
-
         for (int i = n - 2; i >= 0; i--) {
             right[i] = Math.max(right[i + 1], arr[i]);
         }
-
         int water = 0;
-
         for (int i = 0; i < n; i++) {
             water = water + Math.min(left[i], right[i]) - arr[i];
         }
-
         return water;
     }
 }
