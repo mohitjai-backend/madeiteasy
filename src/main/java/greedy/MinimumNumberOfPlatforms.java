afa3c708e59d6de584f5973cb4975e93f4b7bba8
mohitjain2894 <mohit.jain@unicommerce.com>
2021-08-30 00:50:36 +0530

    Minimum number of platforms required

---
+++ src/main/java/greedy/MinimumNumberOfPlatforms.java
@@ -0,0 +1,55 @@
+package main.java.greedy;
+
+import java.util.Arrays;
+
+public class MinimumNumberOfPlatforms {
+    public static void main(String[] args) {
+        int start[] = { 900, 940, 950, 1100, 1500, 1800 };
+        int dep[] = { 910, 1200, 1120, 1130, 1900, 2000 };
+        MinimumNumberOfPlatforms obj = new MinimumNumberOfPlatforms();
+        System.out.println(obj.minNumberOfPlatforms(start, dep));
+    }
+
+    int minNumberOfPlatforms(int start[], int dep[]) {
+        int n = start.length;
+        if (n == 0)
+            return 0;
+
+        Arrays.sort(start);
+        Arrays.sort(dep);
+
+        int i = 1;
+        int j = 0;
+        int platforms_needed_curr = 1;
+        int ans = 1;
+
+        while (i < n && j < n) {
+            if (start[i] < dep[j]) {
+                platforms_needed_curr++;
+                i++;
+            } else {
+                platforms_needed_curr--;
+                j++;
+            }
+
+            ans = Math.max(ans, platforms_needed_curr);
+        }
+
+        return ans;
+    }
+}
+
+// https://www.geeksforgeeks.org/minimum-number-platforms-required-railwaybus-station/
+// https://leetcode.com/problems/meeting-rooms-ii/solution/
+
+
+/*
+A meeting is defined by its start and end times. However, for this specific algorithm, we need to
+treat the start and end times individually. This might not make sense right away because a
+meeting is defined by its start and end times. If we separate the two and treat them individually,
+then the identity of a meeting goes away. This is fine because:
+
+When we encounter an ending event, that means that some meeting that started earlier has
+ended now. We are not really concerned with which meeting has ended. All we need is that
+some meeting ended thus making a room available.
+ */
